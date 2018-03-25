fc502320a426cfa95052b2ae051375c861eb6464
NileshGule <vn_nilesh@yahoo.com>
2018-03-25 22:54:17 +0800

    removed hardcode 8080 port and configured via environment variables

--- TechTalksAPI/Program.cs
+++ TechTalksAPI/Program.cs
@@ -19,7 +19,7 @@ namespace TechTalksAPI
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:8080")
+                // .UseUrls("http://*:8080")
                 .UseStartup<Startup>()
                 .Build();
     }
