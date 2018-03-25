4ea07d0d5a943d338f6e30636985b09f236460d9
NileshGule <vn_nilesh@yahoo.com>
2018-03-25 12:55:09 +0800

    added Database initialization script and Dockerfile with SQL tools

---
+++ TechTalksDB/initialize-database.sql
@@ -0,0 +1,48 @@
+IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'TechTalksDB')
+    BEGIN
+        CREATE DATABASE [TechTalksDB]
+    END;
+
+GO
+
+SELECT Name from sys.Databases
+GO
+
+USE TechTalksDB
+GO
+
+if not exists (select * from sysobjects where name='Categories' and xtype='U')
+    BEGIN
+        CREATE TABLE Categories (id INT, categoryName NVARCHAR(50), description NVARCHAR(100))
+
+        INSERT INTO Categories VALUES(1, 'Meetup', 'Community event organized via meetup');
+        INSERT INTO Categories VALUES(2, 'Free Conference', 'Free Tech Conference');
+        INSERT INTO Categories VALUES(3, 'Paid Conference', 'Paid Tech Conference');
+        INSERT INTO Categories VALUES(4, 'Hackathon', 'Hackathon');
+        INSERT INTO Categories VALUES(5, 'Eventribe', 'Community event organized via Eventribe');
+    END
+GO
+
+if not exists (select * from sysobjects where name='TechTalk' and xtype='U')
+    BEGIN
+        CREATE TABLE TechTalk (id INT, name NVARCHAR(50), category INT)
+
+
+        INSERT INTO TechTalk VALUES (1, 'Scaling Docker Containers', 1); 
+        INSERT INTO TechTalk VALUES (2, 'Azure Container Services', 2);
+    END
+GO
+
+if not exists (select * from sysobjects where name='KeyValue' and xtype='U')
+    BEGIN
+        CREATE TABLE KeyValue ([Key] NVARCHAR(10), [Value] NVARCHAR(100))
+
+        INSERT INTO KeyValue VALUES('GoT', 'American fantasy drama television series');
+        INSERT INTO KeyValue VALUES('MS', 'Microsoft');
+        INSERT INTO KeyValue VALUES('OS', 'Open source');
+        INSERT INTO KeyValue VALUES('Doc', 'Doc-chain aka Blockchain');
+        INSERT INTO KeyValue VALUES('AZ', 'Azure');
+        INSERT INTO KeyValue VALUES('ACS', 'Azure Container Service');
+
+    END
+GO
