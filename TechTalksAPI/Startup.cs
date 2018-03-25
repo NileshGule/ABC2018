228819a782cf92ab24c06fa4d47874cb13c04844
NileshGule <vn_nilesh@yahoo.com>
2018-03-25 23:16:02 +0800

    modified startup to connect to SQL 2017 instance

--- TechTalksAPI/Startup.cs
+++ TechTalksAPI/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TechTalksAPI.Models;
 
 namespace TechTalksAPI
 {
@@ -23,6 +25,9 @@ namespace TechTalksAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<KeyValueContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
