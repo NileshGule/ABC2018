1d9da10cfdad210503c383cd8df9fac31fef4d56
NileshGule <vn_nilesh@yahoo.com>
2018-03-31 13:47:01 +0800

    added logic to check status of minikube cluster before starting

--- Powershell/deployTechTalks.ps1
+++ Powershell/deployTechTalks.ps1
@@ -1,11 +1,30 @@
-Write-Host "Starting deployment of TechTalks application and services"  -ForegroundColor Yellow
+Param(
+    [parameter(Mandatory=$false)][bool]$IsLocalCluster=$true
+)
+
+if($IsLocalCluster){
+
+    $minikubeStatus = minikube status
+
+    Write-Host $minikubeStatus -ForegroundColor Magenta
+
+    $IsStopped =$minikubeStatus.Contains("minikube: Stopped")
+    
+    if($IsStopped)
+    {
+        Write-Host "Starting local minikube cluster" -ForegroundColor Yellow
+        minikube start
+    }
+}
+
+Write-Host "Starting deployment of TechTalks application and services" -ForegroundColor Yellow
 Set-Location ~/projects/ABC2018/Kubernetes
 
 Write-Host "Creating namespace"  -ForegroundColor Yellow
-kubectl apply 00_ABCNamespace.yml
+kubectl apply --filename 00_ABCNamespace.yml
 
 Write-Host "Creating storage class" -ForegroundColor Yellow
-kubectl apply 01_StorageClass.yml
+kubectl apply --filename 01_StorageClass.yml
 
 Write-Host "Creating Persistant Volume Claim" -ForegroundColor Yellow
-kubectl apply 02_PersistantVolumeClaim.yml
+kubectl apply --filename 02_PersistantVolumeClaim.yml
