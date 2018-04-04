ab6cc8a2d6a8c958d165bd05c457a0e54d97aa86
NileshGule <vn_nilesh@yahoo.com>
2018-04-04 16:29:11 +0800

    changed TechTalks web project type from razor to mvc

--- TechTalksDB/initialize-database.sql
+++ TechTalksDB/initialize-database.sql
@@ -1,9 +1,18 @@
-IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'TechTalksDB')
-    BEGIN
-        CREATE DATABASE [TechTalksDB]
-    END;
+-- IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'TechTalksDB')
+--     BEGIN
+--         CREATE DATABASE [TechTalksDB]
+--     END;
+-- GO
 
+IF  EXISTS (SELECT * FROM sys.databases WHERE name = N'TechTalksDB')
+    BEGIN
+        PRINT 'Dropping existing database...'
+        DROP DATABASE [TechTalksDB]
+    END;
 GO
+
+PRINT 'Creating database TechTalksDB...'
+CREATE DATABASE [TechTalksDB]
 
 SELECT Name from sys.Databases
 GO
@@ -13,8 +22,10 @@ GO
 
 if not exists (select * from sysobjects where name='Categories' and xtype='U')
     BEGIN
+        PRINT 'Creating Categories table'
         CREATE TABLE Categories (id INT, categoryName NVARCHAR(50), description NVARCHAR(100))
 
+        PRINT 'Inserting default values in categories table...'
         INSERT INTO Categories VALUES(1, 'Meetup', 'Community event organized via meetup');
         INSERT INTO Categories VALUES(2, 'Free Conference', 'Free Tech Conference');
         INSERT INTO Categories VALUES(3, 'Paid Conference', 'Paid Tech Conference');
@@ -25,9 +36,10 @@ GO
 
 if not exists (select * from sysobjects where name='TechTalk' and xtype='U')
     BEGIN
+        PRINT 'Creating TechTalk table'
         CREATE TABLE TechTalk (id INT, name NVARCHAR(50), category INT)
 
-
+        PRINT 'Inserting default values into TechTalk table'
         INSERT INTO TechTalk VALUES (1, 'Scaling Docker Containers', 1); 
         INSERT INTO TechTalk VALUES (2, 'Azure Container Services', 2);
     END
